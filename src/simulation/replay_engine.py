eaaa94c5543771fedcbde22b8e713e2247707ad1
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 07:31:01 -0500

    Update replay_engine.py

--- src/simulation/replay_engine.py
+++ src/simulation/replay_engine.py
@@ -1,1 +1,15 @@
+import time
+import json
 
+class ReplayEngine:
+
+    def replay(self, memory, delay=1):
+
+        print("\n🔁 REPLAY START\n")
+
+        for i, state in enumerate(memory.history):
+
+            print(f"\n--- Replay Step {i} ---")
+            print(json.dumps(state, indent=2))
+
+            time.sleep(delay)
