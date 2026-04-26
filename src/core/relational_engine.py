fcee89275a8d82e48b2fa8f690165f8e78f6e466
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 11:16:21 -0500

    Update relational_engine.py

--- src/core/relational_engine.py
+++ src/core/relational_engine.py
@@ -1,1 +1,16 @@
+class RelationalEngine:
 
+    def connect(self, processes):
+
+        relations = []
+
+        for p in processes:
+            for q in processes:
+                if p != q:
+                    relations.append({
+                        "from": p["process"],
+                        "to": q["process"],
+                        "type": "interaction"
+                    })
+
+        return relations
