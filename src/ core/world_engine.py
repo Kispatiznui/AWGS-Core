309c56cdab586499ef3b78068be1144f48e88550
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 11:13:24 -0500

    Delete src/ core/world_engine.py

--- src/ core/world_engine.py
+++
@@ -1,8 +0,0 @@
-class WorldEngine:
-
-    def step(self, state):
-
-        for p in state["processes"]:
-            p["state"]["energy"] = p["state"].get("energy", 1) - 0.1
-
-        return state
