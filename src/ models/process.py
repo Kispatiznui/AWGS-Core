7d1432462b0463ae440ca426a068d7eb360dc252
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-25 23:27:35 -0500

    Update process.py

--- src/ models/process.py
+++ src/ models/process.py
@@ -1,1 +1,9 @@
+from pydantic import BaseModel
+from typing import List, Dict, Any
 
+class Process(BaseModel):
+    id: str
+    name: str
+    actors: List[str]
+    environment: str
+    state_variables: Dict[str, Any]
