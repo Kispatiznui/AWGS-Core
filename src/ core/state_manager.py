43a2e6879d715c26f61a5986a9eebb7f9a3a70ea
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-25 23:31:36 -0500

    Update state_manager.py

--- src/ core/state_manager.py
+++ src/ core/state_manager.py
@@ -1,1 +1,18 @@
+from core.world_engine import WorldEngine
+from core.anticipatory_engine import AnticipatoryEngine
 
+class StateManager:
+
+    def __init__(self):
+        self.world_engine = WorldEngine()
+        self.anticipatory_engine = AnticipatoryEngine()
+
+    def step(self, world_state):
+
+        world_state = self.world_engine.step(world_state)
+        predictions = self.anticipatory_engine.predict(world_state)
+
+        return {
+            "world": world_state,
+            "predictions": predictions
+        }
