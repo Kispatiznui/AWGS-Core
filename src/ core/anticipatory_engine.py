ba3e8612ba7be9a83ceb11ccce9b46ec02340e16
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 06:45:10 -0500

    Update anticipatory_engine.py
    "name": "collapse" is used to make it generic

--- src/ core/anticipatory_engine.py
+++ src/ core/anticipatory_engine.py
@@ -1,15 +1,12 @@
 class AnticipatoryEngine:
 
-    def predict(self, world_state):
+    def predict(self, state):
 
-        predictions = []
-
-        for p in world_state.processes:
-
-            if p.name == "survival_process":
-                predictions.append({
-                    "scenario": "migration",
-                    "probability": 0.7
-                })
-
-        return predictions
+        return {
+            "future_scenarios": [
+                {
+                    "name": "collapse",
+                    "probability": 0.6
+                }
+            ]
+        }
