97f6635dd9bece728f36bd1d15213bf4f14f109d
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 06:50:05 -0500

    Update world_engine.py
    improvement in the for loop of dynamic processes

--- src/ core/world_engine.py
+++ src/ core/world_engine.py
@@ -1,17 +1,8 @@
-from models.world_state import WorldState
-
 class WorldEngine:
 
-    def step(self, world_state: WorldState):
+    def step(self, state):
 
-        # simulación básica
-        for rule in world_state.rules:
-            if rule.condition == "water_level == low":
-                for p in world_state.processes:
-                    if p.name == "survival_process":
-                        p.state_variables["thirst"] = "critical"
+        for p in state["processes"]:
+            p["state"]["energy"] = p["state"].get("energy", 1) - 0.1
 
-        world_state.time += 1
-        world_state.history.append(world_state.dict())
-
-        return world_state
+        return state
