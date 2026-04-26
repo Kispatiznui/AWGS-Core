bb01ec05a7db84e9df7fcb3d1a97cf3e2bf1a189
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 07:31:15 -0500

    Update branching_engine.py

--- src/simulation/branching_engine.py
+++ src/simulation/branching_engine.py
@@ -1,1 +1,19 @@
+import copy
 
+class BranchingEngine:
+
+    def generate_branches(self, world_state, variations=3):
+
+        branches = []
+
+        for i in range(variations):
+
+            new_state = copy.deepcopy(world_state)
+
+            # pequeña variación artificial
+            for p in new_state["processes"]:
+                p["state"]["variation"] = i
+
+            branches.append(new_state)
+
+        return branches
