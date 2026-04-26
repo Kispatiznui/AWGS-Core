8d19236bc4f931ce60938ceb08946c70595254e6
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 11:14:52 -0500

    Update rule_engine.py

--- src/core/rule_engine.py
+++ src/core/rule_engine.py
@@ -1,1 +1,13 @@
+class RuleEngine:
 
+    def generate(self, relations):
+
+        rules = []
+
+        for r in relations:
+            rules.append({
+                "if": f"{r['from']}_changes",
+                "then": f"affect_{r['to']}"
+            })
+
+        return rules
