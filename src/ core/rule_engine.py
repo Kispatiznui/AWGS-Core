4b184e9896588260dcd82027c8c2475d22feb432
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-25 23:30:39 -0500

    Update rule_engine.py

--- src/ core/rule_engine.py
+++ src/ core/rule_engine.py
@@ -1,1 +1,16 @@
+from models.rule import Rule
 
+class RuleEngine:
+
+    def generate_rules(self, processes, relations):
+
+        rules = []
+
+        for p in processes:
+            if p.name == "resource_depletion_process":
+                rules.append(Rule(
+                    condition="water_level == low",
+                    effect="increase_survival_pressure"
+                ))
+
+        return rules
