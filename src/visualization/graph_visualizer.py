8d32d407f4601840cb834f2b2001121772cf23f7
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 07:29:07 -0500

    Update graph_visualizer.py

--- src/visualization/graph_visualizer.py
+++ src/visualization/graph_visualizer.py
@@ -1,1 +1,27 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 
+class GraphVisualizer:
+
+    def visualize(self, world_state):
+
+        G = nx.DiGraph()
+
+        # nodos = procesos
+        for p in world_state["processes"]:
+            G.add_node(p["process"])
+
+        # relaciones = edges
+        for r in world_state["relations"]:
+            G.add_edge(r["from"], r["to"], label=r["type"])
+
+        pos = nx.spring_layout(G)
+
+        plt.figure(figsize=(10, 7))
+        nx.draw(G, pos, with_labels=True, node_size=2000, node_color="lightblue")
+        
+        edge_labels = nx.get_edge_attributes(G, 'label')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+        plt.title("AWGS World Graph")
+        plt.show()
