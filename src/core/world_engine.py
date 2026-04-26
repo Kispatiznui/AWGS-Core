9040ceea9a3c277180df0f969752ca392eebd4c2
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-26 11:13:15 -0500

    Update world_engine.py

--- src/core/world_engine.py
+++ src/core/world_engine.py
@@ -1,1 +1,8 @@
+class WorldEngine:
 
+    def step(self, state):
+
+        for p in state["processes"]:
+            p["state"]["energy"] = p["state"].get("energy", 1) - 0.1
+
+        return state
