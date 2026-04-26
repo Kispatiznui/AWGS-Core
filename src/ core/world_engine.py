1ff2aea3ba12459473d2e3ce50ecf321698b8359
Luis Felipe Martínez(Kispat) <martinezbluisf@gmail.com>
2026-04-25 23:31:03 -0500

    Update world_engine.py

--- src/ core/world_engine.py
+++ src/ core/world_engine.py
@@ -1,1 +1,17 @@
+from models.world_state import WorldState
 
+class WorldEngine:
+
+    def step(self, world_state: WorldState):
+
+        # simulación básica
+        for rule in world_state.rules:
+            if rule.condition == "water_level == low":
+                for p in world_state.processes:
+                    if p.name == "survival_process":
+                        p.state_variables["thirst"] = "critical"
+
+        world_state.time += 1
+        world_state.history.append(world_state.dict())
+
+        return world_state
